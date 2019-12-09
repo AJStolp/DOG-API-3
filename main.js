fd171dcd9ee71da2f2961b60badbadcd8c651d29
Anthony Stolp <anthonystolp@anthonys-mbp-2.home>
2019-12-09 09:24:37 -0600

    not appending image

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function getDogImage(breed){
     const options = {method: 'GET'};
     if(breed === ''){
         fetch
-            (`https://dog.ceo/api/breed/hound/images/`, options)
+            (`https://dog.ceo/api/breed/hound/images/random`, options)
                 .then(response => response.json())
                 .then(responseJson => displayResults(responseJson))
                 .catch(error => alert ('Please check your internet connection - the servers may be down ;('))
@@ -23,7 +23,7 @@ function displayResults(responseJson) {
         console.log(responseJson.message);
         $('.data').append(`<p>The breed that you selected can not be located. Please try another breed :)</p>`)
     } else {
-        $('.data').prepend(`<img src="${responseJson.message}" alt='Random Dog Breed' class='img-results'`)
+        $('.data').append(`<img src="${responseJson.message}" alt='Random Dog Breed' class='img-results'`)
     }
 }
 
